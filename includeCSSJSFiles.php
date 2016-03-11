31e2ade3b719b9c2998243e1c2505c314a477ce5
nikhilvinay <vinayjadhav680@gmail.com>
2016-03-11 12:35:06 +0530

    Update includeCSSJSFiles.php

--- includeCSSJSFiles.php
+++ includeCSSJSFiles.php
@@ -1,6 +1,6 @@
 
-<script src="js/bootstrap.min.js"></script>
-<script src="js/bootstrap.js"></script>
-<script src="css/bootstrap.css"></script>
-<script src="css/bootstrap.min.css"></script>
-<script src="css/styles.css"></script>
+<script src="bootstrap.min.js"></script>
+<script src="bootstrap.js"></script>
+<script src="bootstrap.css"></script>
+<script src="bootstrap.min.css"></script>
+<script src="styles.css"></script>
